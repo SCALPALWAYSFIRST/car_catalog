ada68d5cb715f371d7bfb28b31a894d6dba9ea79
SCALPALWAYSFIRST <asd1esher@gmail.com>
2026-02-01 19:16:44 +0300

    feat: add docker-compose and adminer Dockerfile

--- init/01_create_tables.sql
+++ init/01_create_tables.sql
@@ -1,1 +1,8 @@
-SELECT * FROM cars;
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY,
+    brand TEXT NOT NULL,
+    model TEXT NOT NULL,
+    year INTEGER,
+    color TEXT,
+    price NUMERIC(10, 2)
+);
